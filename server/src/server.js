172bf7e761a7811364964fe72d0351fbb486881e
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-07-15 14:25:03 -0300

    Testando Heroku

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,19 @@
-import express from 'express'
-import cors from 'cors'
-import path from 'path'
-import routes from './routes'
-import { errors } from 'celebrate'
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(routes)
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
-app.use(errors())
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var path_1 = __importDefault(require("path"));
+var routes_1 = __importDefault(require("./routes"));
+var celebrate_1 = require("celebrate");
+var app = express_1.default();
+app.use(cors_1.default());
+app.use(express_1.default.json());
+app.use(routes_1.default);
+app.use('/uploads', express_1.default.static(path_1.default.resolve(__dirname, '..', 'uploads')));
+app.use(celebrate_1.errors());
 /* Explicações
 
 const users = [
@@ -51,5 +53,4 @@ app.post('/users', (request, response) => {
     return response.json(user)
 })
 */
-
-app.listen(process.env.PORT || 3333)
+app.listen(3333);
